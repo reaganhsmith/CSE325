d35391b2602a34e5f7e8975d7de072de5cea1e30
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2024-02-18 22:18:35 -0600

    ci: Create models

---
+++ EducationPortal/Components/Models/Course.cs
@@ -0,0 +1,10 @@
+namespace educationPortal.Models;
+
+public class Course
+{
+    public required string Code { get; set; }
+    public required string Title { get; set; }
+    public required string Time { get; set; } 
+    public required string Location { get; set; } 
+    public required string Instructor { get; set; } 
+}
--- EducationPortal/Components/Models/Student.cs
+++ EducationPortal/Components/Models/Student.cs
@@ -1,11 +1,22 @@
-namespace educationPortal.Models
+namespace educationPortal.Models;
+
+public class Student
 {
-    public class Student
-    {
-        public int StudentId { get; set; }
-        public string? Name { get; set; } 
-        public string? Email { get; set; }
-        public string? Phone { get; set; }
-        public string? Major { get; set; }
-    }
+    public int StudentId { get; set; }
+    public required string Name { get; set; } 
+    public required string Email { get; set; }
+    public required string Phone { get; set; }
+    public required string Major { get; set; }
 }
+
+
+
+
+// public class Student
+// {
+//     public int StudentId { get; set; }
+//     public string? Name { get; set; } 
+//     public string? Email { get; set; }
+//     public string? Phone { get; set; }
+//     public string? Major { get; set; }
+// }
