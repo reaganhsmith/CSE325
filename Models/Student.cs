c49f23c0d3dde2d14a5b3ce46c66361b6c837a4d
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2024-02-20 19:05:45 -0600

    ci: Recycled stuff

---
+++ Models/Student.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerBlazorEF.Models;
+
+public class Student {
+    public int StudentId { get; set; }
+    [Required]
+    public string? FirstName { get; set; }
+    [Required]
+    public string? LastName { get; set; }
+    [Required]
+    public string? School { get; set; }
+}
