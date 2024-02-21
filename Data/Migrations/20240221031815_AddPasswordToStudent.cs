5b07d926d0a95095b71d04588ef3826d3c99e9ad
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2024-02-20 21:22:31 -0600

    ci: Remove double pwd from DB

---
+++ Data/Migrations/20240221031815_AddPasswordToStudent.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServerBlazorEF.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordToStudent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConfirmPassword",
+                table: "Students");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConfirmPassword",
+                table: "Students",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1,
+                column: "ConfirmPassword",
+                value: "password1");
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 2,
+                column: "ConfirmPassword",
+                value: "password2");
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 3,
+                column: "ConfirmPassword",
+                value: "password3");
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 4,
+                column: "ConfirmPassword",
+                value: "password4");
+        }
+    }
+}
--- Models/Student.cs
+++ Models/Student.cs
@@ -23,9 +23,6 @@ public class Student
 
     [Required]
     public string? Password { get; set; }
-
-    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-    public string? ConfirmPassword { get; set; }
 }
 
 
